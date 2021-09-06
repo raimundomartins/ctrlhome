7ab49b39ee05b8c0d7aa6d406b467893276ca79b
Raimundo Martins <raimundomartins@protonmail.com>
2021-09-06 13:59:00 +0100

    Add GPLv3 license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/* Copyright © 2018 Raimundo Martins
+
+   This file is part of CtrlHome
+
+   CtrlHome is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   CtrlHome is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with History.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // `error_chain!` can recurse
 #![recursion_limit = "64"]
 
